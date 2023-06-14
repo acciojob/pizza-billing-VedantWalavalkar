7aa07adc290358067d4f05cb975bc745013a5240
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-14 18:09:17 +0530

    Bill generated 1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,16 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price = 300;
+        }
+        else price = 400;
+    }
+    public Pizza(boolean isVeg, boolean dp){
+        if(isVeg){
+            price = 300 + 80 + 70;
+        }
+        else price = 400 + 80 + 70;
     }
 
     public int getPrice(){
@@ -17,18 +27,24 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        price += 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg)
+            price += 70;
+        else price += 120;
     }
 
     public void addTakeaway(){
         // your code goes here
+        price += 20;
     }
 
     public String getBill(){
         // your code goes here
+        bill = "" + price;
         return this.bill;
     }
 }
